45773ac50640907ebbd8e0fa834a51bb114e894d
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-12-01 18:09:21 +1300

    find li in doc

--- scrape.js
+++ scrape.js
@@ -1,22 +1,45 @@
+// THIS WEBSCRAPER ONLY WORKS ON taste.com.au !!!!!!!!!!!
+// DOES NOT WORK ON OTHER WEBSITES
+// @author: Ellaira Torio
+
 import axios from 'axios'
 import cheerio from 'cheerio'
 import promptSync from 'prompt-sync'
+import fs from 'fs'
+import parseIngredients from './util.js'
 
+var ingredientsList = []
+
+var currentSection = 'main'
+// prompt
 const prompt = promptSync()
+const url = prompt('Enter a URL: ')
 
-let url = 'https://www.taste.com.au/recipes/one-pan-cheese-bacon-smothered-chicken-recipe/nrji6hy7?r=dinner&h=Dinner'
-const newURL = prompt('Enter a URL: ')
-
+// GET request to website
 axios
-  .get(newURL)
+  .get(url)
   .then((res) => {
     let $ = cheerio.load(res.data)
     const header = $('.recipe-title-container h1').text()
-    const ingredients = $('#tabIngredients .ingredient-description').text()
-    const text = $('#tabMethodSteps li .recipe-method-step-content').text()
-    console.log(header)
-    console.log(ingredients)
-    console.log(text)
+    let list = []
+    // ingredients that do not have section headings are assumed to be for the 'main' dish
+
+    $('div[id="tabIngredients"]')
+      .find('ul > li')
+      .each(function (index, element) {
+        var text = $(element).text().trim()
+
+        // check if list element is a section
+        if ($(element).attr('class') === 'section-heading') {
+          currentSection = text
+        } else if ($(element).find('div[class="ingredient-description"]').text().trim()) {
+          ingredientsList.push({ [currentSection]: text })
+        }
+      })
+
+    console.log('-----START------')
+    parseIngredients(ingredientsList)
+    console.log('------END------')
   })
   .catch((err) => {
     console.log(err)
---
+++ util.js
@@ -0,0 +1,7 @@
+import _ from 'lodash'
+
+export default function parseIngredients(props) {
+  let finalList = props.map((obj) => {
+    console.log(Object.keys(obj))
+  })
+}
